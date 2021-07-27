4b39df55815e7dfdd76f218a05504897bf78d71b
Elena <42415402+ElenaSamanchuk@users.noreply.github.com>
2021-07-27 15:42:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,6 +39,13 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let decoded='';
+    lengthExpr = expr.length;
+    for (let i = 0; i < lengthExpr; i += 10) {
+        let letter = expr.slice(i, i + 10).replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-');
+        decoded+= MORSE_TABLE[letter] || ' ';
+    }
+    return decoded;
 }
 
 module.exports = {
